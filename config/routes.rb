7024c01f195dabc8df32cbe2e9148fa5bd1c74a8
Seiya-wasabi <se.sato614@gmail.com>
2021-04-07 09:18:25 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/create'
-  get 'books/update'
-  get 'books/destroy'
-  get 'homes/index'
-  get 'homes/show'
-  get 'homes/new'
-  get 'homes/edit'
+get "/" => "homes#index"
+
+resources :homes
+resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
