3f74d5562d2b14436597accb0bf6a25f8ebe5a23
Seiya-wasabi <se.sato614@gmail.com>
2021-04-08 06:32:07 +0000

    [dbへの保存機能を追加]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,18 @@
 class BooksController < ApplicationController
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to"/"
   end
 
   def update
   end
 
   def destroy
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 get "/" => "homes#index"
+post "homes" => "books#create"
 
 resources :homes
 resources :books
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
