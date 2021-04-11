cac9a80e11530bfac4afae1401b071360e719549
Seiya-wasabi <se.sato614@gmail.com>
2021-04-11 12:39:08 +0000

    [今日はここまで　よくできました]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
-    book.save
+    if book.save
     redirect_to "/homes/#{book.id}"
+    else
+      render "new"
+    end
 
     flash[:true] = "Book was successfully created."
   end
