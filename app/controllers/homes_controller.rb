d15d29de331756c506e84421676fbb5821c47fe4
Seiya-wasabi <se.sato614@gmail.com>
2021-04-11 09:30:26 +0000

    編集でエラーがでる

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,18 +1,17 @@
 class HomesController < ApplicationController
   def top
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
 
   def show
-    @book =Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def new
-    
   end
 
   def edit
