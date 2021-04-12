83e801904c7aa5d925c3c6038a378c41c4006788
Seiya-wasabi <se.sato614@gmail.com>
2021-04-12 07:17:33 +0000

    残すはテスtpんpみ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,27 @@
 class BooksController < ApplicationController
   def create
-    book = Book.new(book_params)
-    if book.save
-    redirect_to home_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+   　　 redirect_to home_path(book.id)
     else
-      render homes_path
+     @books = Book.all
+       render template: 'homes/index'
     end
 
     flash[:true] = "Book was successfully created."
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     redirect_to update_book_path(book.id)
+    else
+      @books = Book.all
+    render template: '/homes/edit'
 
     flash[:true1] ="Book was successfully updated."
     end
+  end
 
   def destroy
     book = Book.find(params[:id])
